e1be3083d27d84a3b8a02963455675f6e0312e37
migrenaa <mimonova13@gmail.com>
2022-08-07 14:59:41 -0400

    added event when joining a DAO

--- contracts/communities/HackerDAO.sol
+++ contracts/communities/HackerDAO.sol
@@ -6,10 +6,12 @@ import "./ISWLegacyDAO.sol";
 /// @title HackerDAO
 /// @notice A permissionless HackerDAO for integrating 
 contract HackerDAO is ISWLegacyDAO {
+    event Joined(address member);
     mapping(address => bool) public override isMember;
 
     /// @notice Adds new member to the DAO
     function join() public {
         isMember[msg.sender] = true;
+        emit Joined(msg.sender);
     }
 }
