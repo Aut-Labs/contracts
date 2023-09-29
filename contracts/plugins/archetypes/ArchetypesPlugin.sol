94ac394ac72abc38fd4479aa32e3cf8e496ae9ff
Mikhail <mihanix.eth@gmail.com>
2023-09-29 17:07:32 +0300

    add simpleplugin base & auth

--- contracts/plugins/archetypes/ArchetypesPlugin.sol
+++ contracts/plugins/archetypes/ArchetypesPlugin.sol
@@ -2,18 +2,31 @@
 pragma solidity 0.8.19;
 
 import "./IArchetypesPlugin.sol";
+import "../SimplePlugin.sol";
 
-contract ArchetypesPlugin is IArchetypesPlugin {
+contract ArchetypesPlugin is IArchetypesPlugin, SimplePlugin {
     uint8 public mainArchetype;
     mapping(uint8 => uint256) public archetypeWeightFor;
 
-    function setMainArchetype(uint8 archetype) external {
-        require(mainArchetype == 0, ArchetypeAlreadySet());
-        _validateArchetype(archetype);
-        mainArchetype = archetype;
+    constructor(address dao) {
+        _dao = dao;
+        _deployer = msg.sender;
     }
 
-    function setArchetypeWeightFor(uint8 archetype, uint256 value) external {
+    function setMainArchetype(uint8 archetype) external onlyOwner {
+        require(mainArchetype == 0, ArchetypeAlreadySet());
+        _validateArchetype(archetype);
+        mainArchetype = archetype;   
+        _setActive(true);
+    }
+
+    function setArchetypeWeightFor(
+        uint8 archetype,
+        uint256 value
+    )
+        external
+        onlyOwner 
+    {
         _validateArchetype(archetype);
         archetypeWeightFor[archetype] = value;
     }
