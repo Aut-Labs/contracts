670db12a7690c9b53c943fc1ce443b64c27dbcfa
Mikhail S <mihanix.it@gmail.com>
2023-10-21 16:34:43 +0300

    add archetypes component

---
+++ contracts2/components/ArchetypesComponent.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "../base/ComponentBase.sol";
+
+interface IArchetype {
+    error ArchetypeAlreadySet();
+    error WrongParameter();
+
+    event MainArchetypeSet(uint8);
+    event ArchetypeWeightSet(uint8, uint256);
+
+    function archetype() external view returns(uint8);
+
+    function weightFor(uint8) external view returns(uint256);
+
+    function setArchetype(uint8) external;
+
+    function setWeightFor(uint8, uint256) external;
+}
+
+contract ArchetypeComponent is ComponentBase, IArchetype {
+    uint8 public constant SIZE = 1;
+    uint8 public constant REPUTATION = 2;
+    uint8 public constant CONVICTION = 3;
+    uint8 public constant PERFORMANCE = 4;
+    uint8 public constant GROWTH = 5;
+
+    uint8 public archetype;
+    mapping(uint8 => uint256) public weightFor;
+
+    function setArchetype(uint8 parameter) external {
+        if (archetype != 0) {
+            revert ArchetypeAlreadySet();
+        }
+        _validateParameter(parameter);
+        archetype = parameter;
+    }
+
+    function setWeightFor(
+        uint8 parameter,
+        uint256 value
+    )
+        external 
+    {
+        _validateParameter(parameter);
+        weightFor[parameter] = value;
+    }
+
+    function _validateParameter(uint8 parameter) internal pure {
+        if (parameter > 5 || parameter == 0) {
+            revert WrongParameter();
+        }
+    }
+}
