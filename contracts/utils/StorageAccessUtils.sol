1385d731b56b6817863ce24046c42ee00f29ee66
Carter Carlson <pegahcarter@gmail.com>
2024-08-21 15:48:05 -0700

    feat: storageAccessUtils

--- contracts/utils/StorageAccessUtils.sol
+++ contracts/utils/StorageAccessUtils.sol
@@ -1,2 +1,27 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
+
+contract StorageAccessUtils {
+    address internal _hub;
+    address internal _autID;
+
+    function init_StorageAccessUtils(
+        address hub_,
+        address autID_
+    ) public {
+        _hub = hub_;
+        _autId = autID_;
+    }
+
+    function isAdmin(address who) external view returns (bool) {
+        return IHub(hub).isAdmin(who);
+    }
+
+    function hub() public view returns (address) {
+        return _hub;
+    }
+
+    function autID() public view returns (address) {
+        return _audID;
+    }
+}
