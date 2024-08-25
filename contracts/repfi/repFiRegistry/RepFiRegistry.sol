32833d3e5f1c8cb9f500da2d586535d3e0dd08de
Ben Gyles <bengyles@hotmail.com>
2024-08-26 00:29:35 +0200

    improve comments

--- contracts/repfi/repFiRegistry/RepFiRegistry.sol
+++ contracts/repfi/repFiRegistry/RepFiRegistry.sol
@@ -3,23 +3,40 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
+/// @title RepFiRegistry
+/// @author Āut Labs
+/// @notice This contract keeps track of the plugins used in the Reputation Finance ecosystem and will be used by the pRepFi token mostly as the only transactions allowed for this token is from and to these plugins.
 contract RepFiRegistry is OwnableUpgradeable {
+    /// @notice mapping of plugin addresses and their information
     mapping(address plugin => PluginMeta pluginMeta) private plugins;
 
+    /// @notice emitted when a plugin is added to the registry
+    /// @param contractAddress The contract address of the plugin
     event PluginAdded(address contractAddress);
+
+    /// @notice emitted when a plugin is removed from the registry
+    /// @param contractAddress The contract address of the plugin
     event PluginRemoved(address contractAddress);
 
+    /// @notice provides more information about a plugin
+    /// @param description The description of the plugin
+    /// @param enabled Whether the plugin is enabled
     struct PluginMeta {
         string description;
         bool enabled;
     }
 
+    /// @notice RepFiRegistry contract initializer
+    /// @param initialOwner The initial owner of the contract
     function initialize(address initialOwner) external initializer {
         __Ownable_init(initialOwner);
     }
 
     uint256[50] private __gap;
 
+    /// @notice Registers a plugin, only callable by the owner of the contract
+    /// @param contractAddress the address of the contract
+    /// @param _description The description of the plugin
     function registerPlugin(address contractAddress, string calldata _description) external onlyOwner {
         require(contractAddress != address(0), "contractAddress must be a valid address");
         require(plugins[contractAddress].enabled == false, "plugin is already registered");
@@ -28,6 +45,8 @@ contract RepFiRegistry is OwnableUpgradeable {
         emit PluginAdded(contractAddress);
     }
 
+    /// @notice removes a plugin, only callable by the owner of the contract
+    /// @param contractAddress the address of the contract
     function removePlugin(address contractAddress) external onlyOwner {
         require(contractAddress != address(0), "contractAddress must be a valid address");
         require(plugins[contractAddress].enabled == true, "plugin does not exist");
@@ -36,11 +55,15 @@ contract RepFiRegistry is OwnableUpgradeable {
         emit PluginRemoved(contractAddress);
     }
 
+    /// @notice returns whether the plugin is enabled
+    /// @param contractAddress the address of the contract
     function isPlugin(address contractAddress) external view returns (bool) {
         return plugins[contractAddress].enabled;
     }
 
-    function getPluginDescription(address _contractAddress) external view returns (string memory) {
-        return plugins[_contractAddress].description;
+    /// @notice returns the plugin description
+    /// @param contractAddress the address of the contract
+    function getPluginDescription(address contractAddress) external view returns (string memory) {
+        return plugins[contractAddress].description;
     }
 }
