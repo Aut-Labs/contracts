fdc6f2a1a1a97ecceb1db0b2ff114b444a68f246
Carter Carlson <pegahcarter@gmail.com>
2024-08-27 14:56:04 -0700

    wip: taskFactory

--- contracts/tasks/TaskFactory.sol
+++ contracts/tasks/TaskFactory.sol
@@ -4,29 +4,31 @@ pragma solidity ^0.8.20;
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract TaskFactory is Initializable {
-    uint128 currentSumCreatedContributionPoints;
-    uint128 currentSumActiveContributionPoints;
-    uint128 currentSumGivenContributionPoints;
-    uint128 currentSumRemovedContributionPoints;
+
+    uint128 public currentSumPoints;
 
     address public hub;
     address public membership;
 
-    struct Task {
-        bytes32 interactionId;
+    struct MetaTask {
+        bytes32 taskId;
         uint256 role;
-        uint32 periodIdDeployed;
         uint32 startDate;
         uint32 endDate;
-        uint32 weight;
+        uint32 points;
         uint128 quantity;
         string description;
         // TODO: further identifiers
     }
+
     mapping(bytes32 => bool) public taskId;
 
     function initialize(address _hub, address _membership) external initializer {
         hub = _hub;
         membership = _membership;
+    }
+
+    function createTask(MetaTask calldata task) external {
+        
     }
 }
