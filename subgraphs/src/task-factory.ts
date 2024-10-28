ad6eb88e37c73bad7cca58f9c354ddcc2a1f7dfd
mrtmeeseeks <taulant.disha@ablera.com>
2024-10-28 21:02:51 +0200

    latest changes on the contribution schemas

--- subgraphs/src/task-factory.ts
+++ subgraphs/src/task-factory.ts
@@ -1,25 +1,16 @@
 import { CreateContribution } from '../generated/TaskFactory/TaskFactory'
 import { Contribution } from '../generated/schema'
-import { ethereum, Bytes } from '@graphprotocol/graph-ts'
 
 export function handleCreateContribution(event: CreateContribution): void {
   let contribution = new Contribution(event.params.contributionId.toHexString())
 
-  let decoded = ethereum.decode(
-    "(bytes32,string,uint256,uint256,uint8,uint8,string)",
-    event.params.encodedContribution
-  )
-
-  if (decoded !== null) {
-    let tuple = decoded.toTuple()
-    contribution.taskId = tuple[0].toBytes()
-    contribution.role = tuple[1].toString()
-    contribution.startDate = tuple[2].toBigInt()
-    contribution.endDate = tuple[3].toBigInt()
-    contribution.points = tuple[4].toI32()
-    contribution.quantity = tuple[5].toI32()
-    contribution.description = tuple[6].toString()
-  }
+  contribution.taskId = event.params.taskId
+  contribution.role = event.params.role
+  contribution.startDate = event.params.startDate
+  contribution.endDate = event.params.endDate
+  contribution.points = event.params.points
+  contribution.quantity = event.params.quantity
+  contribution.descriptionId = event.params.descriptionId
 
   contribution.creator = event.params.sender
   contribution.blockNumber = event.block.number
