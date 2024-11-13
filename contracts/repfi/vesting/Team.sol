b5e7462bd7eea39fe56449a4d947b4a7e1817c58
Ben Gyles <bengyles@hotmail.com>
2024-11-13 20:51:33 +0100

    fix Info-2-1

--- contracts/repfi/vesting/Team.sol
+++ contracts/repfi/vesting/Team.sol
@@ -6,8 +6,6 @@ import {TokenVesting} from "./TokenVesting.sol";
 contract Team is TokenVesting {
     // settings for this vesting contract
 
-    // max amount to vest in this contract
-    uint256 constant TOTAL_VEST_AMOUNT = 5000000 ether; // may not be needed as we're using the balance to check this in TokenVesting
     // duration of the vesting period in seconds
     uint256 constant DURATION = 6 * 30 days; // 6 months
     // duration of a slice period for the vesting in seconds
